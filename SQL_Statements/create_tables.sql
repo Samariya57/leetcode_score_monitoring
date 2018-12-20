a6223257dc1ff464c145ce52b032dfc71af7353d
Mariia Danilenko <Samariya57@gmail.com>
2018-12-20 14:33:14 -0500

    Update create_tables.sql

--- SQL_Statements/create_tables.sql
+++ SQL_Statements/create_tables.sql
@@ -4,11 +4,11 @@ DROP TABLE IF EXISTS active_fellows CASCADE;
 
 CREATE TABLE IF NOT EXISTS active_fellows (
 	leetcode_user_name				varchar(50)	PRIMARY KEY ,
-	first_name						varchar(50),
-	last_name						varchar(50),
-	email							varchar(50),
-	session_location			    varchar(50),
-	session_code				    varchar(20)
+	first_name					varchar(50),
+	last_name					varchar(50),
+	email						varchar(50),
+	session_location			    	varchar(5),
+	session_code				    	varchar(5)
 );
 
 
@@ -17,12 +17,12 @@ CREATE TABLE IF NOT EXISTS active_fellows (
 -- Create leetcode_records table if not exists;
 DROP TABLE IF EXISTS leetcode_records CASCADE;
 
-CREATE TABLE IF NOT EXISTS active_fellows (
+CREATE TABLE IF NOT EXISTS leetcode_records (
 	user_name						varchar(50)	REFERENCES active_fellows(leetcode_user_name),
 	num_solved						INTEGER,
 	num_accepts						INTEGER,
-	num_submissions			    	INTEGER,
-	solved_percentage				NUMERIC,
-	finished_contests				INTEGER,
+	num_submissions			    			INTEGER,
+	solved_percentage					NUMERIC,
+	finished_contests					INTEGER,
 	record_date						DATE
 );
