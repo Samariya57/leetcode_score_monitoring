a37cca092340b42d22166b5a280a71ce0bfaf3c4
Heng2j <heng2j@gmail.com>
2019-01-13 23:19:02 -0500

    updated leetcode_spider.py to upload data to PostgreSQL DB

--- src/Scraper/leetCode_spider/leetCode_spider/spiders/leetcode_spider.py
+++ src/Scraper/leetCode_spider/leetCode_spider/spiders/leetcode_spider.py
@@ -1,8 +1,34 @@
 # -*- coding: utf-8 -*-
+
+#!/usr/bin/env python3
+# leetcode_spider.py
+# ---------------
+# Author: Zhongheng Li
+# Start Date: 12-20-18
+# Last Modified Date: 1-13-19
+
+
+"""
+This python script will be execute while running the spider script.
+
+"""
+
+# System modules
+from datetime import date
+import configparser
+import os
+
+# 3rd party modules
 import scrapy
 import pandas as pd
-from datetime import date
+from sqlalchemy import create_engine
 
+
+# Set up DB configs file path
+projectPath = os.getcwd()
+DB_configs_ini_file_path = "/leetCode_spider/spiders/db_configs.ini"
+
+print(projectPath + DB_configs_ini_file_path)
 
 
 class LeetCodeSpider(scrapy.Spider):
@@ -16,16 +42,39 @@ class LeetCodeSpider(scrapy.Spider):
         self.userIndex = 0
         self.lenOfURLs = len(self.start_urls)
 
+    def getSQL_DB_Engine(self, filePath = None):
 
+        config = configparser.ConfigParser()
+        config.read(projectPath + DB_configs_ini_file_path)
+
+
+        DB_TYPE = config['DB_Configs']['DB_TYPE']
+        DB_DRIVER = config['DB_Configs']['DB_DRIVER']
+        DB_USER = config['DB_Configs']['DB_USER']
+        DB_PASS = config['DB_Configs']['DB_PASS']
+        DB_HOST = config['DB_Configs']['DB_HOST']
+        DB_PORT = config['DB_Configs']['DB_PORT']
+        DB_NAME = config['DB_Configs']['DB_NAME']
+        POOL_SIZE = config['DB_Configs']['POOL_SIZE']
+        TABLENAME = config['DB_Configs']['TABLENAME']
+        SQLALCHEMY_DATABASE_URI = '%s+%s://%s:%s@%s:%s/%s' % (DB_TYPE, DB_DRIVER, DB_USER,
+                                                              DB_PASS, DB_HOST, DB_PORT, DB_NAME)
+        ENGINE = create_engine(
+            SQLALCHEMY_DATABASE_URI, echo=False)
+
+        return ENGINE
 
     def parse(self, response):
 
         badge_onprogress_bar = response.selector.xpath("//span[@class='badge progress-bar-success']/text()").extract()
 
         has_contest = False if len(badge_onprogress_bar) <= 6 else True
+        #
+        # columns = ['email', 'user_name', 'first_name', 'last_name', 'num_solved', 'num_accepts','num_submissions', 'accepted_percentage',
+        #            'finished_contests', 'record_date']
 
-        columns = ['email', 'user_id', 'first_name', 'last_name', 'num_solved', 'num_accepts','num_submissions', 'accepted_percentage',
-                   'finished_contests', 'date']
+        columns = ['user_name', 'num_solved', 'num_accepts','num_submissions', 'accepted_percentage',
+                   'finished_contests', 'record_date']
 
         user_model = None
 
@@ -34,7 +83,7 @@ class LeetCodeSpider(scrapy.Spider):
             user_model = {
 
                 # 'email': product_id,
-                'user_id': self.start_urls[self.userIndex].split('/')[-1],
+                'user_name': self.start_urls[self.userIndex].split('/')[-1],
 
                 # 'first_name': product_id,
                 # 'last_name': product.title,
@@ -53,7 +102,7 @@ class LeetCodeSpider(scrapy.Spider):
                 # 'global_ranking' : badge_onprogress_bar[2].split('\n')[1].strip(),
                 #
 
-                'date': str(date.today())
+                'record_date': str(date.today())
 
             }
 
@@ -62,7 +111,7 @@ class LeetCodeSpider(scrapy.Spider):
             user_model = {
 
                 # 'email': product_id,
-                'user_id': self.start_urls[self.userIndex].split('/')[-1],
+                'user_name': self.start_urls[self.userIndex].split('/')[-1],
 
                 # 'first_name': product_id,
                 # 'last_name': product.title,
@@ -75,7 +124,7 @@ class LeetCodeSpider(scrapy.Spider):
                 # 'points': int(badge_onprogress_bar[3].split('\n')[1].strip()),
 
                 'finished_contests': int(badge_onprogress_bar[0].split('\n')[1].strip()),
-                'date': str(date.today())
+                'record_date': str(date.today())
 
             }
 
@@ -90,7 +139,15 @@ class LeetCodeSpider(scrapy.Spider):
 
             print("export fellows.csv")
 
-            df.to_csv("leetCode_spider/data/" + str(date.today()) + "_fellows.csv", sep=",")
+            # Build the sql ULR for SqlAlchemy
+            # sql_url = "sqlite:////" + os.path.join(basedir, "story_writer.db")
+
+            df.to_csv("leetCode_spider/data/" + str(date.today()) + "_fellows.csv", sep=",", index=False)
+
+            engine = self.getSQL_DB_Engine()
+
+
+            df.to_sql(name='leetcode_records', con=engine, if_exists='append', index=False)
 
 
 
--- src/airflow/airflow_home/dags/scraping_scheduler_dag.py
+++ src/airflow/airflow_home/dags/scraping_scheduler_dag.py
@@ -1,5 +1,20 @@
+#!/usr/bin/env python3
+# scraping_scheduler_dag.py
+# ---------------
+# Author: Zhongheng Li
+# Start Date: 12-20-18
+# Last Modified Date: 1-13-19
+
+
+"""
+This is a dag to schedule the scraping script to run daily at 11:00pm EST from Monday to Friday
+
+"""
+
+# System modules
 import datetime as dt
 
+# 3rd party modules
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
 
